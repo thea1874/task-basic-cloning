074f81adac60841c4d41ce0f06e388e117bb361d
Jonas Holbech <jh@jonasholbech.dk>
2021-02-03 10:38:17 +0100

    basic astructure

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
+const gitHop = {
+  name: "GitHop",
+  img: "githop.png",
+  description: "FooBar's Venopm Ale",
+};
+//This is the object that you should use to "populate" the template
+
 //Step 1: Chose the <template>'s content
-const templateElement = document.querySelector("#myTemplate").content;
 
 //Step 2: Make a "clone"
-const myClone = templateElement.cloneNode(true);
 
-//Step 3: Change the content
-myClone.querySelector("h1").textContent = "Hi Mom";
-//...
+//Step 3: Change the content of the clone
+//use the data from the object above
 
 //Step 4: Chose the new "parent" element
-const parentElement = document.querySelector("body");
+//Append it to main
 
 //Step 5: Add (Append) the clone to the DOM
-parentElement.appendChild(myClone);
