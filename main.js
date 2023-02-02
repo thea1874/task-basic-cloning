7a1cf1bbc2a188f6446a94cb18d1c15aa5ec81c0
Thea Nielsen <thea1874@kea.dk>
2023-02-02 13:42:43 +0100

    js

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const gitHop = {
   description: "FooBar's Venom Ale",
 };
 
+function showProduct(gitHop){
 //Step 1: Chose the <template>'s content
 const template = document.querySelector("#gitHopTemplate").content;
 
@@ -13,7 +14,7 @@ const clone = template.cloneNode(true);
 //Step 3: Change the content of the clone
 //use the data from the object above (you can do this when the rest is working)
 
-clone.querySelector("img").textContent = gitHop.img
+clone.querySelector("img").textContent = gitHop.img;
 clone.querySelector("p").textContent = gitHop.description;
 
 //Step 4: Chose the new "parent" element
@@ -22,4 +23,7 @@ clone.querySelector("p").textContent = gitHop.description;
 document.querySelector("#GitHop").appendChild(clone);
 
 //Step 5: Add (Append) the clone to the DOM
+
+parent.appendChild(clone);
+}
 
