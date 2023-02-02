4130545a36ae31a6c1220cc3c3136684fc40385e
Thea Nielsen <thea1874@kea.dk>
2023-02-02 13:30:57 +0100

    js kode

--- main.js
+++ main.js
@@ -6,13 +6,20 @@ const gitHop = {
 };
 
 //Step 1: Chose the <template>'s content
+const template = document.querySelector("#gitHopTemplate").content;
 
 //Step 2: Make a "clone"
-
+const clone = template.cloneNode(true);
 //Step 3: Change the content of the clone
 //use the data from the object above (you can do this when the rest is working)
+
+clone.querySelector("img").textContent = gitHop.img
+clone.querySelector("p").textContent = gitHop.description;
 
 //Step 4: Chose the new "parent" element
 //Append it to main
 
+document.querySelector("#GitHop").appendChild(clone);
+
 //Step 5: Add (Append) the clone to the DOM
+
